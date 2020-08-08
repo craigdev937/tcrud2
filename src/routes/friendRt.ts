756f46e1d454f92dff6e7a26a206ffac2b7d05f6
Craig <indigomx9@gmail.com>
2020-08-08 17:40:23 -0400

    Updated the Config files and Procfile.

--- src/routes/friendRt.ts
+++ src/routes/friendRt.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { indexHome, CreateFriend, RenderFriend, ListAllFriends, FindOneFriend, 
-    EditFriend, DeleteFriend } from "../controllers/friendCon";
+import { indexHome, CreateFriend, RenderFriend, ListAllFriends, 
+    FindOneFriend,     EditFriend, DeleteFriend } from "../controllers/friendCon";
 import { check } from "express-validator";
 
 export const friendRt: express.Router = express.Router();
-    friendRt.post("/create", check("name")., CreateFriend);
+    friendRt.post("/create", check("name"), CreateFriend);
     friendRt.get("/create", RenderFriend);
     friendRt.get("/list", ListAllFriends);
     friendRt.get("/edit/:id", FindOneFriend);
